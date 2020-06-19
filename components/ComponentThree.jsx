77dcc7dd5ccc277efef8c1d70381baeae155fa22
Spencer Mefford <smefford@nav.com>
2020-06-19 11:37:07 -0600

    Use router outside MemoryRouter

--- components/ComponentThree.jsx
+++ components/ComponentThree.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import useFlowController from '../hooks/useFlowController';
 
 const ComponentThree = () => {
+  const history = useHistory();
   const gotoNext = useFlowController();
   return (
     <>
       <div>ComponentThree</div>
       <br />
       <div>
+        <button
+          type="button"
+          onClick={() => {
+            history.push('/foo');
+          }}
+        >
+          Go somewhere else
+        </button>
         <button
           type="button"
           onClick={() => {
--- components/SomeOuterFlow.jsx
+++ components/SomeOuterFlow.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { MemoryRouter, Route } from 'react-router-dom';
+import { MemoryRouter, Route, Switch, useHistory } from 'react-router-dom';
 import _ from 'lodash';
 import FlowController from './common/FlowController';
 import SomeInnerFlow from './inner-flow/SomeInnerFlow';
@@ -11,13 +11,17 @@ const getStepIndicator = (index, length) =>
   _.times(length, (i) => `${i === index ? 'X' : 'O'} `);
 
 const SomeOuterFlow = () => {
-  const [currentStep, setCurrentStep] = useState(0);
+  const history = useHistory();
+
   const steps = [
     { path: '/component-one' },
     { path: '/component-two' },
     { path: '/some-inner-flow' },
     { path: '/component-three' },
   ];
+  const startingStep = 0;
+
+  const [currentStep, setCurrentStep] = useState(startingStep);
 
   const handleComplete = () => {
     // eslint-disable-next-line no-alert
@@ -28,14 +32,14 @@ const SomeOuterFlow = () => {
   return (
     <div>
       <h2>{getStepIndicator(currentStep, steps.length)}</h2>
-      <MemoryRouter initialEntries={[steps[0].path]}>
+      <MemoryRouter initialEntries={[steps[startingStep].path]}>
         <FlowController
           steps={steps}
           onStepChange={setCurrentStep}
           onComplete={handleComplete}
         >
           {({ gotoNext }) => (
-            <>
+            <Switch>
               <Route exact path="/component-one">
                 <ComponentOne />
               </Route>
@@ -48,7 +52,14 @@ const SomeOuterFlow = () => {
               <Route exact path="/component-three">
                 <ComponentThree />
               </Route>
-            </>
+              <Route
+                render={({ location }) => {
+                  // This will error there isn't a router at the root of our app, but that's ok
+                  // because this is just a POC.
+                  history.push(location.pathname); // This is a tricky way for inner components to navigate to routes outside MemoryRouter
+                }}
+              />
+            </Switch>
           )}
         </FlowController>
       </MemoryRouter>
